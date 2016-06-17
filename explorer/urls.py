9ee53f1917c52da090310181f121f65ffd40b801
j0hnsmith <info@whywouldwe.com>
2016-06-17 13:18:00 +0100

    string args to url are deprecated

--- explorer/urls.py
+++ explorer/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url
-from explorer.views import QueryView, CreateQueryView, PlayQueryView, DeleteQueryView, ListQueryView, ListQueryLogView
+from explorer import views
 
 urlpatterns = [
-    url(r'(?P<query_id>\d+)/$', QueryView.as_view(), name='query_detail'),
-    url(r'(?P<query_id>\d+)/download$', 'explorer.views.download_query', name='query_download'),
-    url(r'(?P<query_id>\d+)/csv', 'explorer.views.view_csv_query', name='query_csv'),
-    url(r'(?P<pk>\d+)/delete$', DeleteQueryView.as_view(), name='query_delete'),
-    url(r'new/$', CreateQueryView.as_view(), name='query_create'),
-    url(r'play/$', PlayQueryView.as_view(), name='explorer_playground'),
-    url(r'csv$', 'explorer.views.download_csv_from_sql', name='generate_csv'),
-    url(r'schema/$', 'explorer.views.schema', name='explorer_schema'),
-    url(r'logs/$', ListQueryLogView.as_view(), name='explorer_logs'),
-    url(r'format/$', 'explorer.views.format_sql', name='format_sql'),
-    url(r'^$', ListQueryView.as_view(), name='explorer_index'),
+    url(r'(?P<query_id>\d+)/$', views.QueryView.as_view(), name='query_detail'),
+    url(r'(?P<query_id>\d+)/download$', views.download_query, name='query_download'),
+    url(r'(?P<query_id>\d+)/csv', views.view_csv_query, name='query_csv'),
+    url(r'(?P<pk>\d+)/delete$', views.DeleteQueryView.as_view(), name='query_delete'),
+    url(r'new/$', views.CreateQueryView.as_view(), name='query_create'),
+    url(r'play/$', views.PlayQueryView.as_view(), name='explorer_playground'),
+    url(r'csv$', views.download_csv_from_sql, name='generate_csv'),
+    url(r'schema/$', views.schema, name='explorer_schema'),
+    url(r'logs/$', views.ListQueryLogView.as_view(), name='explorer_logs'),
+    url(r'format/$', views.format_sql, name='format_sql'),
+    url(r'^$', views.ListQueryView.as_view(), name='explorer_index'),
 ]
